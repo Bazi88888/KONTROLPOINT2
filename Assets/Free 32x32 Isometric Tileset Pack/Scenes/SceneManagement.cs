b1e0bb4a84fb3db30dc17d70dacfd3fb87a27ca0
Bazi88888 <NovikovNO24@spb.ithub.ru>
2025-10-07 19:36:22 +0300

    ADD PROJECT

---
+++ Assets/Free 32x32 Isometric Tileset Pack/Scenes/SceneManagement.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string Valera2009)
+    {
+        SceneManager.LoadScene(Valera2009);
+    }
+}
